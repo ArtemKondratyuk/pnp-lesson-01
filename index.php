b6954697f46c5f1c0fab8e99e114614f9f7ce9f2
ArtemKondratyuk <118299767+ArtemKondratyuk@users.noreply.github.com>
2024-05-18 12:02:30 +0300

    echo operator and comment
    udied the echo operator, comment

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    
+<?php 
+echo "<b>Hello</b> World!";
+echo '</br>"';
+echo "You are the best!";
+echo '"';
+
+// Comment
+# comment
+
+/*
+
+Comment 
+
+*/
+
+?>
+
+</body>
+</html>
